a7bfd70d99052fb04458c99f4a1c10150f456b57
Andreydenisov7788 <asasa88881974@mail.ru>
2022-09-03 00:43:15 +0300

    условный оператор 2.2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        int age = 11;
-        if (age >= 18) {
-            System.out.println("Поздравляем пользователя");
-        } else {
-            System.out.println(" Нужно подождать с поздравлением ");
-        }
+        int personAge = 20;
+        if (personAge > 23) {
+            System.out.println("человек закончил универ");
+        } else if (personAge > 17) {
+            System.out.println("Закончил школу и может отправлятся в универ");
+        } else if (personAge >= 7) {
+            System.out.println("Ходит в школу");
+        } else { System.out.println(" пока не куда не ходит "); }
     }
-}
+    }
