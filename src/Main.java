4a03e75d6a77562276ac6ac3dc3482521bca28b3
Andreydenisov7788 <asasa88881974@mail.ru>
2022-09-02 21:45:59 +0300

    условный оператор 3.1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int age = 11;
+        if (age >= 18) {
+            System.out.println("Поздравляем пользователя");
+        } else {
+            System.out.println(" Нужно подождать с поздравлением ");
+        }
+    }
+}
