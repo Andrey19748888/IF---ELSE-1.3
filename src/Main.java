64f7addbfe8b1e790ebcd3d2c0ba8308d2557e18
Andreydenisov7788 <asasa88881974@mail.ru>
2022-09-04 16:16:55 +0300

    условный оператор 7.3.1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        int age = 11;
-        if (age >= 18) {
-            System.out.println("Поздравляем пользователя");
-        } else {
-            System.out.println(" Нужно подождать с поздравлением ");
+        int age = 35;
+        if (age >= 2 && age < 7) {
+            System.out.println("Если возраст чедовека равен от 2 до 6 то ему нужно ходить в детский сад");
+        } else if (age >= 7 && age < 19) {
+            System.out.println("Если возраст чедовека равен от 7 до 18 то ему нужно ходить в школу");
+        }
+        else if (age >= 19 && age < 24) {
+            System.out.println("Если возраст чедовека равен от 18 до 23 то ему нужно ходить в университет");
+        } else {System.out.println("Если возраст чедовека больше 24 то ему нужно ходить на работу");
         }
     }
 }
