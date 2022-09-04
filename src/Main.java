9a821cf427ec468f25e194ba219dc1643fde63e5
Andreydenisov7788 <asasa88881974@mail.ru>
2022-09-04 23:46:22 +0300

    условный оператор7.3.2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        int age = 11;
-        if (age >= 18) {
-            System.out.println("Поздравляем пользователя");
+        int age = 15;
+        if (age < 5) {
+            System.out.println("Ребенок не может кататься на аттракционе");
+        }
+         else if ( age >= 5 && age < 14 ) {
+            System.out.println("Ребенок может кататься только в сопровождении взрослого");
+            
         } else {
-            System.out.println(" Нужно подождать с поздравлением ");
+            System.out.println("Может кататься без сопровождения взрослого");
         }
     }
 }
