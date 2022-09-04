7e4ed5042b69f3f2e75908acc764e8938c41750c
Andreydenisov7788 <asasa88881974@mail.ru>
2022-09-04 15:29:20 +0300

    условный оператор 2.3

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        int age = 11;
-        if (age >= 18) {
-            System.out.println("Поздравляем пользователя");
-        } else {
-            System.out.println(" Нужно подождать с поздравлением ");
+
+            int people = 101;
+            int placesInCar = 102;
+            int sitStil = 60;
+            int standStill = placesInCar - sitStil;
+            if (people == placesInCar ) {
+                System.out.println("мест нет вообще");
+            }
+            else if (people < sitStil){
+                System.out.println("  Сидячие места есть");
+            }
+            else   {
+                System.out.println(" Есть только стоячие места ");
         }
     }
 }
